fee277bb45982206001c218e2671221ab41dea4a
jvillarejo <arzivian87@gmail.com>
2018-04-14 12:11:10 -0300

    removed dep

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,7 +2,6 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'minitest/autorun'
 require 'active_record'
 require 'embedded'
-require 'byebug'
 
 ActiveRecord::Base.establish_connection adapter: "sqlite3", database: ":memory:"
 
